e885adb44f68208c7a29a24b9b2796f899bb4c25
Lewis Liu <lewisliuusa@outlook.com>
2020-07-23 22:47:20 -0700

    Added switch and route sample

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const Hats = () => (
+	<div>
+		<h1>Hats</h1>
+	</div>
+);
+
 function App() {
 	return (
 		<>
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/hats" component={Hats} />
+			</Switch>
 		</>
 	);
 }
